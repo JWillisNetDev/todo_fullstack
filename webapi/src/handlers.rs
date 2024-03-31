a5a92b8584c02ba9f7528b433271a5cfb5a55b5a
Joshua Willis <joshua.willis@intellitect.com>
2024-03-31 14:25:02 -0700

    Encapsulate handlers

---
+++ webapi/src/handlers.rs
@@ -0,0 +1,92 @@
+use crate::db::{
+    models::{Todo, UpdateTodo},
+    repositories::DbConnection,
+    Pool,
+};
+use axum::{
+    extract::Path,
+    http::StatusCode,
+    routing::{delete, get, post, put},
+    Json, Router,
+};
+
+fn internal_error(err: String) -> (StatusCode, String) {
+    (StatusCode::INTERNAL_SERVER_ERROR, err.to_string())
+}
+
+pub trait RouteTodoExt {
+    fn route_todo(&self) -> Self;
+}
+
+impl RouteTodoExt for Router<Pool> {
+    fn route_todo(&self) -> Self {
+        self.to_owned()
+            .route("/todo/list", get(get_todos))
+            .route("/todo/:id", get(get_todo))
+            .route("/todo/create", post(create_todo))
+            .route("/todo/:id", put(update_todo))
+            .route("/todo/:id", delete(delete_todo))
+    }
+}
+
+pub async fn get_todos(db: DbConnection) -> Result<Json<Vec<Todo>>, (StatusCode, String)> {
+    let todos = DbConnection::get_todos(db).await.map_err(internal_error)?;
+    Ok(Json(todos))
+}
+
+pub async fn get_todo(
+    db: DbConnection,
+    Path(id): Path<i32>,
+) -> Result<Json<Option<Todo>>, (StatusCode, String)> {
+    let result = DbConnection::get_todo(db, id)
+        .await
+        .map_err(internal_error)?;
+    Ok(Json(result))
+}
+
+pub async fn create_todo(
+    db: DbConnection,
+    title: String,
+) -> Result<Json<Todo>, (StatusCode, String)> {
+    let todo = DbConnection::create_todo(db, title.as_str())
+        .await
+        .map_err(internal_error)?;
+
+    Ok(Json(todo))
+}
+
+pub async fn update_todo<'a>(
+    db: DbConnection,
+    Path(id): Path<i32>,
+    Json(update_todo): Json<UpdateTodo>,
+) -> Result<Json<Option<Todo>>, (StatusCode, String)> {
+    let todo = DbConnection::update_todo(db, id, update_todo)
+        .await
+        .map_err(internal_error)?;
+
+    if todo.is_none() {
+        Err((
+            StatusCode::NOT_FOUND,
+            format!("No todo item exists with id `{id}`"),
+        ))
+    } else {
+        Ok(Json(todo))
+    }
+}
+
+pub async fn delete_todo(
+    db: DbConnection,
+    Path(id): Path<i32>,
+) -> Result<(), (StatusCode, String)> {
+    let res = DbConnection::delete_todo(db, id)
+        .await
+        .map_err(internal_error)?;
+    if res {
+        Ok(())
+    } else {
+        Err((
+            StatusCode::NOT_FOUND,
+            format!("No todo item exists with id `{id}` or todo item could not be deleted."),
+        ))
+    }
+}
--- webapi/src/main.rs
+++ webapi/src/main.rs
@@ -1,19 +1,11 @@
-use crate::db::{
-    models::{Todo, UpdateTodo},
-    repositories::DbConnection,
-};
-use axum::{
-    extract::Path,
-    http::StatusCode,
-    response::Json,
-    routing::{delete, get, post, put},
-    Router,
-};
+use crate::handlers::RouteTodoExt;
+use axum::Router;
 use diesel_async::pooled_connection::AsyncDieselConnectionManager;
 use std::net::SocketAddr;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 pub mod db;
+mod handlers;
 
 #[tokio::main]
 async fn main() {
@@ -31,76 +23,10 @@ async fn main() {
     let config = AsyncDieselConnectionManager::<diesel_async::AsyncPgConnection>::new(db_url);
     let pool = bb8::Pool::builder().build(config).await.unwrap();
 
-    let app = Router::new()
-        .route("/todo/list", get(get_todos))
-        .route("/todo/:id", get(get_todo))
-        .route("/todo/create", post(create_todo))
-        .route("/todo/:id", put(update_todo))
-        .route("/todo/:id", delete(delete_todo))
-        .with_state(pool);
+    let app = Router::new().route_todo().with_state(pool);
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     tracing::debug!("listening on {addr}");
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
     axum::serve(listener, app).await.unwrap();
-}
-
-fn internal_error(err: String) -> (StatusCode, String) {
-    (StatusCode::INTERNAL_SERVER_ERROR, err.to_string())
-}
-
-async fn get_todos(db: DbConnection) -> Result<Json<Vec<Todo>>, (StatusCode, String)> {
-    let todos = DbConnection::get_todos(db).await.map_err(internal_error)?;
-    Ok(Json(todos))
-}
-
-async fn get_todo(
-    db: DbConnection,
-    Path(id): Path<i32>,
-) -> Result<Json<Option<Todo>>, (StatusCode, String)> {
-    let result = DbConnection::get_todo(db, id)
-        .await
-        .map_err(internal_error)?;
-    Ok(Json(result))
-}
-
-async fn create_todo(db: DbConnection, title: String) -> Result<Json<Todo>, (StatusCode, String)> {
-    let todo = DbConnection::create_todo(db, title.as_str())
-        .await
-        .map_err(internal_error)?;
-
-    Ok(Json(todo))
-}
-
-async fn update_todo<'a>(
-    db: DbConnection,
-    Path(id): Path<i32>,
-    Json(update_todo): Json<UpdateTodo>,
-) -> Result<Json<Option<Todo>>, (StatusCode, String)> {
-    let todo = DbConnection::update_todo(db, id, update_todo)
-        .await
-        .map_err(internal_error)?;
-
-    if todo.is_none() {
-        Err((
-            StatusCode::NOT_FOUND,
-            format!("No todo item exists with id `{id}`"),
-        ))
-    } else {
-        Ok(Json(todo))
-    }
-}
-
-async fn delete_todo(db: DbConnection, Path(id): Path<i32>) -> Result<(), (StatusCode, String)> {
-    let res = DbConnection::delete_todo(db, id)
-        .await
-        .map_err(internal_error)?;
-    if res {
-        Ok(())
-    } else {
-        Err((
-            StatusCode::NOT_FOUND,
-            format!("No todo item exists with id `{id}` or todo item could not be deleted."),
-        ))
-    }
 }
